4c5f2ef070787c989d355a82d6bfaab54d53519c
JMBellidoDev <mbcjose2@gmail.com>
2024-07-13 13:28:31 +0200

    Config Eureka Client

--- springboot-servicio-config-server/src/main/java/formacion/microservicios/app/SpringbootServicioConfigServerApplication.java
+++ springboot-servicio-config-server/src/main/java/formacion/microservicios/app/SpringbootServicioConfigServerApplication.java
@@ -2,10 +2,12 @@ package formacion.microservicios.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class SpringbootServicioConfigServerApplication {
 
   public static void main(String[] args) {
