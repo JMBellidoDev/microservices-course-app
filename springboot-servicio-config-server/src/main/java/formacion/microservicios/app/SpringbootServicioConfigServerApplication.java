db43766217e29a39cb3a9d0c346a137cb59bed11
JMBellidoDev <mbcjose2@gmail.com>
2024-07-13 11:09:25 +0200

    Update repo

--- springboot-servicio-config-server/src/main/java/formacion/microservicios/app/SpringbootServicioConfigServerApplication.java
+++ springboot-servicio-config-server/src/main/java/formacion/microservicios/app/SpringbootServicioConfigServerApplication.java
@@ -2,12 +2,14 @@ package formacion.microservicios.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringbootServicioConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootServicioConfigServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootServicioConfigServerApplication.class, args);
+  }
 
 }
